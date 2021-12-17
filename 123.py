7f48ad0e8ae6cbd9963a726bf4614477c94d90dd
Ayton Huang <a22980688@gmail.com>
2021-12-17 11:16:06 +0800

    first commit

--- 123.py
+++ 123.py
@@ -1,4 +1,8 @@
-c = input('輸入今天攝氏溫度: ',)
-c = float(c)
-f = c * 9 / 5 +32
-print('今天的華氏溫度為: ', f ,'度')
+age = input("请输入年龄： ")
+age = int(age)
+if age >= 20:          #if 解释为如果，当结果为True时，会执行区块内的程式码
+	print("你可以投票") 
+else:                  #else 解释为否则，if判断的结果为False时，才会执行else区块的程式码
+	print("你还不能投票。")
+
+
