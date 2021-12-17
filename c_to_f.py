508432344316d466bc43b38a066489fbf440a0dc
Ayton Huang <a22980688@gmail.com>
2021-12-17 11:16:41 +0800

    first commit

--- c_to_f.py
+++ c_to_f.py
@@ -1,9 +1,9 @@
 c = input('请输入温度：')
 c = float(c)
 f = c * 9 / 5 + 32
-print("今日的华氏温度为： ", f , "度F")
+print("今日的华氏温度为： ", f , "°F")
 
 ff = input("输入华氏温度: ")
 ff = float(ff)
 cc = (ff - 32) *5 / 9 
-print("今日的温度为： ",cc , "度C")
+print("今日的温度为： ",cc , "°C")
